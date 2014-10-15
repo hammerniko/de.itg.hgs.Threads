041ae712b477e068f605be2c7166db0db2602a0d
hr <hammer.nikolai@googlemail.com>
2014-10-15 07:46:51 +0200

    Kommentar added

--- Threads/src/a02_Ampel/StartAmpel.java
+++ Threads/src/a02_Ampel/StartAmpel.java
@@ -3,6 +3,7 @@ package a02_Ampel;
 public class StartAmpel {
 	
 	public static void main(String[] args) {
+		//Threadobjekte erzeugen
 		Ampel a = new Ampel("A", Ampel.ROT, 1000, 500, 2000, 700);
 		Ampel b = new Ampel("B", Ampel.BLINKEN, 1000, 500, 2000, 700);
 		Ampel c = new Ampel("C", Ampel.GRUEN, 3000, 1500, 2500, 1700);
