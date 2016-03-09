67951f98ae01e8c57b1739e3728c7ece2d6375e4
nh <n.hammer@online.de>
2016-03-09 11:12:05 +0100

    konflikt gelöst

--- Threads/src/d04_NotifySampleMitGui/AnimationThread.java
+++ Threads/src/d04_NotifySampleMitGui/AnimationThread.java
@@ -33,7 +33,7 @@ class AnimationThread implements Runnable {
 	private void warte() {
 		try {
 
-		    Thread.sleep(500);
+		    Thread.sleep(50);
 		} catch (InterruptedException e) {
 		    e.printStackTrace();
 		}
