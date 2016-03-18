a3dc752d19add7b304059d65f396f9365ce0dab0
nh <n.hammer@online.de>
2016-03-18 10:29:10 +0100

    reset

--- Threads/src/d04_NotifySampleMitGui/AnimationThread.java
+++ Threads/src/d04_NotifySampleMitGui/AnimationThread.java
@@ -32,6 +32,7 @@ class AnimationThread implements Runnable {
 
 	private void warte() {
 		try {
+<<<<<<< HEAD
 
 		    Thread.sleep(50);
 		} catch (InterruptedException e) {
@@ -39,3 +40,11 @@ class AnimationThread implements Runnable {
 		}
 	}
 }
+=======
+		    Thread.sleep(50);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+	}
+}
+>>>>>>> branch 'master' of https://github.com/hammerniko/de.itg.hgs.Threads.git
