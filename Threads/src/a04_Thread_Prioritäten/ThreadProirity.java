e35297cc96abc5fe70a771e34964e0896c77638e
nh <n.hammer@online.de>
2018-11-06 20:58:11 +0100

    Files modified

--- Threads/src/a04_Thread_Prioritäten/ThreadProirity.java
+++ Threads/src/a04_Thread_Prioritäten/ThreadProirity.java
@@ -8,19 +8,30 @@ import a03_Thread_unterbrechen.Unterbrechung;
  * Mit setPriority(Thread.NORM_PRIORITY)setzt man die standard Priorität eines Threads. (5)
  * Mit getPriority() ermittelt man die Priorität eines Threads.
  * Die Prioritäten entsprechen Zahlen von 1 bis 10
+ * 
+ * Eine höhere Priorität eines Threads in Java erzwingt nicht automatisch eine Ausführung vor einem 
+ * Thread mit einer wichtigeren Priorität. Dies hängt vom Verhalten des
+ * jeweiligen Schedulers ab.
+ * 
+ * So startet dieses PRogramm oft mit einem anderen Thread. Nicht immer mit dem mit der höchsten Priorirät.
+ * 
  */
 
 public class ThreadProirity extends Thread {
 
 	private String name;
-	
-	public ThreadProirity(String name) {
+	private int dauer;
+	private int workTime;
+
+	public ThreadProirity(String name, int dauer) {
 		this.name = name;
+		this.dauer = dauer;
+		workTime = 5;
 	}
 
 	@Override
 	public void run() {
-		
+		System.out.println();
 		while (true) {
 
 			// Wenn unterbrochen wurde, breche die Schleife ab
@@ -28,9 +39,13 @@ public class ThreadProirity extends Thread {
 				break;
 
 			// Wenn nicht unterbrochen wurde mach weiter
-			System.out.println(name+ "Thread läuft mit priorität"+getPriority()+"\n");
+			System.out.print(name + dauer + "|");
+			dauer = dauer - workTime;
+			if (dauer <= 0)
+				break;
+
 			try {
-				Thread.sleep(250);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// Wenn während der Sleep-Phase unterbrochen
 				// wurde und deshalb eine Exception ausgelöst wurde.
@@ -39,34 +54,22 @@ public class ThreadProirity extends Thread {
 			}
 
 		}
-		System.out.println("Thread beendet");
-	}
-	
-	
-	public static void main(String[] args) {
-		System.out.println("Main Thread gestartet");
-		
-		ThreadProirity t1 = new ThreadProirity("A");
-		ThreadProirity t2 = new ThreadProirity("B");
-		
-		t1.setPriority(MAX_PRIORITY);
-		t2.setPriority(MIN_PRIORITY);
-		
-		t1.start();
-		t2.start();
-		
-	
-		
-		try {
-			Thread.sleep(4000);
-		} catch (InterruptedException e) { }
-		
-		t1.interrupt();
-		System.out.println("Main Thread zuende");
-		
-		
-		
 	}
 
+	public static void main(String[] args) {
+
+		ThreadProirity a = new ThreadProirity("A", 20);
+		ThreadProirity b = new ThreadProirity("B", 30);
+		ThreadProirity c = new ThreadProirity("C", 40);
+
+		a.setPriority(MAX_PRIORITY);
+		b.setPriority(MIN_PRIORITY);
+		c.setPriority(NORM_PRIORITY);
+
+		a.start();
+		b.start();
+		c.start();
+
+	}
 
 }
