71df4f6d05a9033c68869f6f83ecde7cd67d5145
nh <n.hammer@online.de>
2018-11-06 20:17:44 +0100

    Kommentar zur Unterbrechung mit interrupt() added

--- Threads/src/a03_Thread_unterbrechen/Unterbrechung.java
+++ Threads/src/a03_Thread_unterbrechen/Unterbrechung.java
@@ -1,5 +1,16 @@
 package a03_Thread_unterbrechen;
 
+/*
+ * Die Methoden stop() und suspend() zum unterbechen von Threads von aussen sind
+ * veraltet und sollten nicht mehr verwendet werden.
+ * Statt dessen kann ein Flag mit der Methode interrupt() ausgelöst werden.
+ * Dabei wird versucht den Thread zu unterbrechen. Ist dieser gerade nicht aktiv, wird eine
+ * InterruptedException ausgelöst. Beim Auffangen dieser Exception kann man dann nochmals die interrupt()
+ * Methode aufrufen. Wird nun mit isInterrupted() geprüft, liefert die Methode true 
+ * und der Thread kann wie z.B. hier mit break; abgebrochen werden.
+ * 
+ */
+
 public class Unterbrechung extends Thread {
 
 	@Override
