a1f1bc962db232a4675d33e6b6abeb651a7fb301
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2018-12-14 14:58:33 +0100

    Files mti KD erweitert

--- Threads/src/Z7_HP2011A3_Pacman/FressPunkt.java
+++ Threads/src/Z7_HP2011A3_Pacman/FressPunkt.java
@@ -1,9 +1,29 @@
 package Z7_HP2011A3_Pacman;
 
-public class FressPunkt {
+public class FressPunkt extends SpielFigur{
 
-	public FressPunkt() {
+	private boolean gefressen;
+	
+	
+	public FressPunkt(int pPosX, int pPosY) {
 		// TODO Auto-generated constructor stub
+	}
+
+
+	public boolean gibGefressen() {
+		return gefressen;
+	}
+
+
+	public void setzeGefressen(boolean pGefressen) {
+		this.gefressen = pGefressen;
+	}
+
+
+	@Override
+	public void zeichne(ZeichenFlaeche pZF) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Threads/src/Z7_HP2011A3_Pacman/OberFlaeche.java
+++ Threads/src/Z7_HP2011A3_Pacman/OberFlaeche.java
@@ -4,12 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 
-public class OberFlaeche extends JFrame {
+public class OberFlaeche extends JFrame implements KeyListener{
+	
+	//
+	private ZeichenFlaeche zeichenFlaeche;
 	
 	//Deklaration für Bidirektionale Assoziation
 	//Gui-Steuerung
@@ -88,8 +95,43 @@ public class OberFlaeche extends JFrame {
 		
 		
 		setContentPane(contentPane);
-		
 		setVisible(true);
+		
+		btStart.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				clickStart();
+				
+			}
+		});
+		
+	}
+	
+	public void ereignisTastatur() {
+		
+	}
+	
+	public void clickStart() {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
